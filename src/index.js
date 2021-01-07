8e0fa3ba4b017da1ea1a0e8e45cf5c3e0376844d
pdulce <pedro.dulce@babel.es>
2021-01-07 13:34:36 +0100

    first review

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+//settings
+var app = express();
+app.set("port", 9080);
+app.set("view engine", "ejs");
+app.engine('html', require('ejs').renderFile);
+app.set("views", path.join(__dirname, "views"));
+
+//server port definition
+app.listen(app.get("port"), (req, res) => {
+  console.log("...Server works, folk!! Listening at port " + app.get("port"));
+});
+
+
+console.log("Server starting...");
+
+// routes
+app.use(require('./routes/router'));
---
+++ src/routes/router.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    console.log("Server attends at HTTP-METHOD = GET");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.render("login.html", {title: 'Bienvenido a login'});
+});
+
+router.get("/contact", (req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.render("contact.html", {title: 'Bienvenido a contactos'});
+});
+
+router.get("/gedeones", (req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.render("gedeones.html", {title: 'Bienvenido a consulta de Gedeones!'});
+});
+
+router.get("/ini", (req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.render("pral.ejs", {title: 'Bienvenido a consulta de Gedeones!'});
+});
+
+module.exports = router;//exportamos el alias router
