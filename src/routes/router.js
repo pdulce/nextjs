93de3dbcbab696d4f2a18ba30791039df600b06f
pdulce <pedro.dulce@babel.es>
2021-01-10 18:36:44 +0100

    stable version

--- src/routes/router.js
+++ src/routes/router.js
@@ -33,9 +33,9 @@ router.get("/contact", (req, res) => {
     res.render("contact.html", {title: 'Contacte vía e-mail', entry: 3});
 });
 
-router.get("/reportingCUBO", (req, res) => {
+router.get("/reporting", (req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.render("reportingCUBO.html", {title: 'Genere el contenido para informe bimensual de su proyecto', content: '', entry: 4});
+    res.render("reporting.html", {title: 'Informes del contrato CDISM', entry: 4});
 });
 
 router.get("/investmentResearch", (req, res) => {
@@ -46,6 +46,54 @@ router.get("/investmentResearch", (req, res) => {
 router.get("/gedeones", (req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.render("gedeones.html", {title: 'Consulta de Gedeones', entry: 61});
+});
+
+router.post("/genCertMensualAT", async (req, res) => {
+  
+  var mesCertificado = moment().subtract(1, 'month').format("MM");
+  var data = [];
+  records = await genCertifMensualAT(data);
+
+  var bloques = new StringBuffer();
+  var proyectoAux = '';
+  var numPeticionesMes = 0;
+
+  for (let i = 0; i < records.length; i++) {
+    var splitter_ = records[i].split("|");
+    var proyecto_ = splitter_[0];
+    var codGedeon_ = splitter_[1];
+    var descGedeon_ = splitter_[2];
+    if (proyectoAux == '' || proyecto_ != proyectoAux){
+      /*if (proyectoAux != ''){
+        var numPetsEnLiteral = (numPeticionesMes == 0)?' ninguna actividad en ': ((numPeticionesMes == 1)?' una actividad en ':` ${numPeticionesMes} actividades en `);
+        bloques.append("\nResumen ").append("=>").append(numPetsEnLiteral).append(monthList[parseInt(mesCertificado)-1] );
+        bloques.append("\n");
+      }*/
+      numPeticionesMes = 0;
+      proyectoAux = proyecto_;
+                 
+      bloques.append("\n\n");
+      bloques.append(proyectoAux);
+      bloques.append("\n\n");
+    }
+    
+    //llenamos el bloque actual
+    bloques.append("-  ").append(codGedeon_).append(" - ").append(descGedeon_);
+    bloques.append("\n"); 
+    
+    numPeticionesMes++;
+          
+  }//for
+  /*var numPetsEnLiteral = (numPeticionesMes == 0)?' ninguna actividad en ': ((numPeticionesMes == 1)?' una actividad en ':` ${numPeticionesMes} actividades en `);
+  bloques.append("\nResumen ").append("=>").append(numPetsEnLiteral).append(monthList[parseInt(mesCertificado)-1] );
+  bloques.append("\n");*/
+  
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");//letra del informe: Trebuchet MS 9 (negrita para proyecto y mes)
+  res.write("Número total de actividades realizadas en " + monthList[parseInt(mesCertificado)-1] + ": " + records.length);
+  res.write("\n");
+  res.write(bloques.toString());
+  res.end();
+  
 });
 
 router.post('/genCUBO', async (req, res) => {
@@ -126,10 +174,10 @@ router.post('/genCUBO', async (req, res) => {
   
   });
 
-  function genReportCUBO(arr) {
+function genReportCUBO(arr) {
     var db = new sqlite3.Database(databaseFile);
     //strftime('%d/%m/%Y',date(i99.fecha_estado_modif)) as fecha
-    var myQuery = "SELECT strftime('%m',date(i99.fecha_estado_modif)) as mes, i99.Proyecto_ID as Proyecto, i99.id as Cod_GEDEON, i99.Titulo as desc FROM incidenciasProyecto i99 LEFT OUTER JOIN subdireccion s74 ON s74.id=i99.Unidad_origen LEFT OUTER JOIN servicio s68 ON s68.id=i99.Area_origen  WHERE  ((date(i99.Fecha_de_tramitacion) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))  OR (date(i99.fecha_estado_modif) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))) AND (  (i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Estructurado'  OR i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Orientado a Objecto') OR  (i99.Tipo LIKE '%Entrega%' AND i99.Area_destino LIKE 'Desarrollo Gestionado%')) GROUP BY i99.Proyecto_ID, i99.fecha_estado_modif ORDER BY i99.Proyecto_ID, i99.fecha_estado_modif asc";
+    var myQuery = "SELECT strftime('%m',date(i99.fecha_estado_modif)) as mes, i99.Proyecto_ID as Proyecto, i99.id as Cod_GEDEON, i99.Titulo as desc FROM incidenciasProyecto i99 LEFT OUTER JOIN subdireccion s74 ON s74.id=i99.Unidad_origen LEFT OUTER JOIN servicio s68 ON s68.id=i99.Area_origen  WHERE  ((date(i99.Fecha_de_tramitacion) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))  OR (date(i99.fecha_estado_modif) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))) AND (  (i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Estructurado'  OR i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Orientado a Objecto') OR  (i99.Tipo LIKE '%Entrega%' AND i99.Area_destino LIKE 'Desarrollo Gestionado%')) GROUP BY i99.Proyecto_ID, i99.fecha_estado_modif ORDER BY i99.Proyecto_ID, i99.fecha_estado_modif, i99.id asc";
     return new Promise((resolve,reject)=>{
       db.all(myQuery, (err, rows) => {
         if(err){
@@ -146,7 +194,37 @@ router.post('/genCUBO', async (req, res) => {
 
     });//end of Promise
 
-   };//end of genReportCUBO
+};//end of genReportCUBO
 
+
+function genCertifMensualAT(arr) {
+  var db = new sqlite3.Database(databaseFile);
+  var myQuery = "SELECT i99.Proyecto_ID as Proyecto, i99.id as Cod_GEDEON, i99.Titulo as desc FROM incidenciasProyecto i99 LEFT OUTER JOIN subdireccion s74 ON s74.id=i99.Unidad_origen LEFT OUTER JOIN servicio s68 ON s68.id=i99.Area_origen  WHERE ("+
+  "(date(i99.Fecha_de_tramitacion) between date('now','start of month','-1 month') AND date('now','start of month','-1 day'))  OR"+
+  "(date(i99.fecha_estado_modif) between date('now','start of month','-1 month') AND date('now','start of month','-1 day'))"+
+  ") AND "+
+    "("+
+      "(i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Estructurado'  OR i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Orientado a Objecto') OR "+
+      "(i99.Tipo LIKE '%Entrega%' AND i99.Area_destino LIKE 'Desarrollo Gestionado%')"+
+    ")"+
+    "AND i99.Proyecto_ID IN ('FMAR', 'FOMA', 'SANI', 'FAM2', 'FAMA', 'FRMA', 'FOM2', 'WSRT', 'WISM', 'WBOF')"+
+    "ORDER BY i99.Proyecto_ID, i99.fecha_estado_modif asc";
+  return new Promise((resolve,reject)=>{
+    db.all(myQuery, (err, rows) => {
+      if(err){
+        return console.error(err.message);
+      }
+      rows.forEach(function (row, index) {
+        //${index}->
+        arr.push(`${row.Proyecto} | ${row.Cod_GEDEON} | ${row.desc}`);
+      });
+      resolve(arr);
+    });//end of db.all
+    
+    db.close();
+
+  });//end of Promise
+
+};//end of genReportCUBO
 
 module.exports = router;//exportamos el alias router
