c833d72329ced7672e69ade0d72c8b117f8b7ccc
pdulce <pedro.dulce@babel.es>
2021-01-11 14:55:42 +0100

    .

--- src/routes/router.js
+++ src/routes/router.js
@@ -176,12 +176,74 @@ router.post("/genCUBO", async (req, res) => {
 
 
 router.post("/caducanSoon", async (req, res) => {
-  //TODO: este informe lo tienes cais hecho: parametriza con un text-input el número de días del periodo en que caducan peticiones
+  let diasfin = req.body.diasfin;
+  console.log(`días próximos en que terminan gedeones: ${diasfin}`);
+    
+  var data = [];
+  records = await genReportCaducadas(diasfin, data);
   
-  let dias = req.body.diasfin;
+  var bloques = new StringBuffer();
+  var contadorBloques = 0;
+  var proyectoAux = '', areaAux = '', fechaFinAux = '';
+  
+  for (let i = 0; i < records.length; i++) {
+    var splitter_ = records[i].split("|");
+    var proyecto_ = splitter_[0];
+    //var origendpto_ = splitter_[1];
+    //var centroOrigen_ = splitter_[2];
+    var codGedeon_ = splitter_[3];
+    var fechaFin_ = splitter_[4];
+    var tipo_ = splitter_[5];
+    var area_ = splitter_[6];
+    var situacion_ = splitter_[7];
+    //var fecFechaNecesidad_ = splitter_[8];  
+    var descGedeon_ = splitter_[9];
+    //`${row.Proyecto} | ${row.area_origen} | ${row.centro_origen} | ${row.Cod_GEDEON} | ${row.fecFechaFin} | ${row.tipo} | ${row.destino} | ${row.situacion} | ${row.fecNecesidad} | ${row.desc}`);
+ 
+
+    if (proyectoAux == '' || proyecto_ != proyectoAux){
+              
+      contadorBloques++;
+      proyectoAux = proyecto_;
+      areaAux = area_;
+      fechaFinAux = fechaFin_;
+            
+      bloques.append("\n\n");
+      bloques.append(proyectoAux);
+      bloques.append("\n\n");
+      bloques.append("\t" + areaAux);
+      bloques.append("\n\n");
+      bloques.append("\t\t" + fechaFinAux);
+      bloques.append("\n\n");
+    }
+    
+    //llenamos el bloque actual
+    if (area_ != areaAux){
+      areaAux = area_;
+      bloques.append("\n");
+      bloques.append("\t" + areaAux);
+      bloques.append("\n\n");
+      bloques.append("\t\t" + fechaFinAux);
+      bloques.append("\n\n");
+    }
+    if (fechaFin_ != fechaFinAux){
+      fechaFinAux = fechaFin_;
+      bloques.append("\n");
+      bloques.append("\t\t" + fechaFinAux);//formatea la fecha con la librería moment
+      bloques.append("\n\n");
+    }
+    
+    bloques.append("\t\t\t- ").append(situacion_).append(" - ").append(tipo_).append(" - ").append(codGedeon_).append(" - ").append(descGedeon_);
+    bloques.append("\n");
+    
+  }//for
+
+  //console.log("num. bloques: " + contadorBloques);
+  
   res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-  res.write("En construcción");
+  res.write(`Número total de actividades que van a finalizar en los próximos ${diasfin} días => ${records.length}`);
   res.write("\n");
+  res.write(bloques.toString());
   res.end();
 });
 
@@ -328,10 +390,10 @@ function genReportPPTX(fechaDesde, arr) {
 
 };//end of genReportCUBO
 
-function genReportCUBO(dias, arr) {
+function genReportCaducadas(dias, arr) {
   var db = new sqlite3.Database(databaseFile);
-  //strftime('%d/%m/%Y',date(i99.fecha_estado_modif)) as fecha
-  var myQuery = "SELECT strftime('%m',date(i99.fecha_estado_modif)) as mes, i99.Proyecto_ID as Proyecto, i99.id as Cod_GEDEON, i99.Titulo as desc FROM incidenciasProyecto i99 LEFT OUTER JOIN subdireccion s74 ON s74.id=i99.Unidad_origen LEFT OUTER JOIN servicio s68 ON s68.id=i99.Area_origen  WHERE  ((date(i99.Fecha_de_tramitacion) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))  OR (date(i99.fecha_estado_modif) between date('now','start of month','-2 month') AND date('now','start of month', '-1 day'))) AND (  (i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Estructurado'  OR i99.Area_destino LIKE '7201 17G L2 ISM ATH Análisis Orientado a Objecto') OR  (i99.Tipo LIKE '%Entrega%' AND i99.Area_destino LIKE 'Desarrollo Gestionado%')) GROUP BY i99.Proyecto_ID, i99.fecha_estado_modif ORDER BY i99.Proyecto_ID, i99.fecha_estado_modif, i99.id asc";
+  var myQuery = `SELECT i99.Proyecto_ID as Proyecto, s74.nombre as area_origen, s68.nombre as centro_origen, i99.id as Cod_GEDEON, i99.Titulo as desc, i99.Des_fecha_prevista_fin as fecFechaFin, i99.Tipo as tipo, i99.Area_destino as destino, i99.Estado as situacion, i99.Fecha_de_necesidad as fecNecesidad FROM incidenciasProyecto i99 LEFT OUTER JOIN subdireccion s74 ON s74.id=i99.Unidad_origen LEFT OUTER JOIN servicio s68 ON s68.id=i99.Area_origen  WHERE date(i99.Des_fecha_prevista_fin) between date('now') AND date('now', '+${dias} day') AND (i99.Des_fecha_real_fin is null OR date(i99.Des_fecha_real_fin) < date('now')) AND i99.Estado <> 'Petición de trabajo finalizado' ORDER BY i99.Proyecto_ID, i99.Area_destino asc`;
+  
   return new Promise((resolve,reject)=>{
     db.all(myQuery, (err, rows) => {
       if(err){
@@ -339,7 +401,7 @@ function genReportCUBO(dias, arr) {
       }
       rows.forEach(function (row, index) {
         //${index}->
-        arr.push(`${row.Proyecto} | ${row.mes} | ${row.Cod_GEDEON} | ${row.desc}`);
+        arr.push(`${row.Proyecto} | ${row.area_origen} | ${row.centro_origen} | ${row.Cod_GEDEON} | ${row.fecFechaFin} | ${row.tipo} | ${row.destino} | ${row.situacion} | ${row.fecNecesidad} | ${row.desc}`);
       });
       resolve(arr);
     });//end of db.all
@@ -349,5 +411,7 @@ function genReportCUBO(dias, arr) {
   });//end of Promise
 
 };//end of genReportCUBO
+
+
 
 module.exports = router;//exportamos el alias router
