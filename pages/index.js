ab481869e54c55187b6e55ad3a7e9188902271b0
pdulce <pedro.dulce@babel.es>
2021-01-23 15:39:23 +0100

    subido desde mi workstation

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentación útil--> Este proyecto se va a ir alimentando de la rama local-academy</h3>
+            <h3>Documentación útil: Este proyecto se va a ir alimentando de la rama local-academy</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
         </div>
